faeeee9bbe7518d8eefd8e342d56bc4a5391b8c2
Vernon Mullen <vernonmu@gmail.com>
2018-06-14 00:41:07 -0500

    added form. apparently much different from angularjs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  angularForm = new FormGroup({
+    name: new FormControl()
+  });
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angularForm = this.fb.group({
+      name: ['', Validators.required],
+    });
+  }
+
+  onSubmit(form: any): void {
+    console.log('you submitted value: ', form)
+  }
+
+  // postInquiry(form).then(function(response){
+  //       $scope.form.firstname='',
+  //       $scope.form.lastname='',
+  //       $scope.form.phone='',
+  //       $scope.form.email='',
+  //       $scope.form.typeofproj='',
+  //       $scope.form.comment=''
+  //       $scope.messages = 'Your form has been submitted!'
+  //       $timeout(function () {
+  //         $scope.messages = '';
+  //       }, 3000);
+  // });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
